c2bc5d2a9077a8892730a72b0979795a9f862151
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-09-05 12:21:03 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1="Priyanka";
+
+        Name n1=new Name() {
+            @Override
+            public boolean isStartingWithA(String name) {
+                return name.charAt(0)=='A';
+            }
+        };
+        System.out.println("n1.isStartingWithA(s1) = " + n1.isStartingWithA(s1));
+    }
+}
